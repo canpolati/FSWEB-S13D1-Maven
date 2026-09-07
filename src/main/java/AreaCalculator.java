2d6fcf7dd98622167b4ede795e39845911f6e43b
canpolati <canpolat.isik@gmail.com>
2026-09-08 00:06:37 +0300

    README görevleri tamamlandı

---
+++ src/main/java/AreaCalculator.java
@@ -0,0 +1,16 @@
+public class AreaCalculator {
+
+    public static double area(double width, double height) {
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
+    }
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
+    }
+}
